83e24e9bac296c8f94e77b91b3ecd4090cda3e9a
Hossin <94375287+qwe2qwee@users.noreply.github.com>
2023-12-10 04:48:43 +0300

    working on sign-up & sign-in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import SigninForm from "./_auth/forms/SigninForm";
 import "./globals.css";
+import { Route, Routes } from "react-router-dom";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
-    <div>
-      <h1 className=' text-6xl'>Helloo worlddd!</h1>
-    </div>
+    <main className='flex h-screen'>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
+        </Route>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   );
 };
 
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const SignupForm = () => {
+  return (
+    <div>
+      SignupForm
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default SignupForm;
